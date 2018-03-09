f494b958b5bbebc4919e6c37a37d488e0c1df91f
Felipe Navarro <felinavarro94@gmail.com>
2018-03-09 03:57:06 -0300

    bash script is now printed by cfgMaker.py

--- NtupleMaker/CFG/cfgMaker.py
+++ NtupleMaker/CFG/cfgMaker.py
@@ -5,42 +5,52 @@ import sys
 fileName = sys.argv[1]
 mfile = open(fileName,'r')
 fileList = mfile.readlines()
-
-
+fBash = open("run.sh", 'w')
+print>>fBash, '#!/bin/bash'
 numFiles = len(fileList)
 	
 print numFiles
 
 outputFile = "electron"
-
-for i in range(1,numFiles/100 +1):
-	fOutName = outputFile + str(i+1)+ '_cfg'
+count = 0
+for i in range(0,numFiles/100 +1):
+	print i
+	fOutName = outputFile + str(i)+ '_cfg.py'
 	fOut = open(fOutName, 'w')
-			
-	print 'import FWCore.ParameterSet.Config as cms'	
-	print 'import FWCore.ParameterSet.Config as cms'
-	print 'process = cms.Process("Demo")'
-	print 'process.load("FWCore.MessageService.MessageLogger_cfi")'
-	print 'process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )'
-	print 'process.source = cms.Source("PoolSource",'
-	print '  # replace \'myfile.root\' with the source file you want to use'
-	print    'fileNames = cms.untracked.vstring('
+	
+	print>> fBash, + "cmsRun" fOutName +'> run' +str(i) + '.log & 2>&1'
+	print >> fBash, tailf 'run' +str(i) + '.log & 2>&1'
+	
+	print>> fOut, 'import FWCore.ParameterSet.Config as cms'	
+	print>> fOut, 'import FWCore.ParameterSet.Config as cms'
+	print>> fOut, 'process = cms.Process("Demo")'
+	print>> fOut, 'process.load("FWCore.MessageService.MessageLogger_cfi")'
+	print>> fOut, 'process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )'
+	print>> fOut, 'process.source = cms.Source("PoolSource",'
+	print>> fOut, '  # replace \'myfile.root\' with the source file you want to use'
+	print >> fOut,   'fileNames = cms.untracked.vstring('
 	lim = 100
 	if i == numFiles/100:
 		lim = numFiles%100 
-	for j in range(1,lim):
-		if j < lim-1:
-			print '\''+ fileList[(i*100)+j][:-1] +'\','
+	for j in range(0,lim ):
+		#print j
+		count = count +1
+		if j < lim:
+			print>> fOut, '\''+ fileList[(i*100)+j ][:-1] +'\','
 		else:
-			print '\''+ fileList[(i*100)+j][:-1] +'\''
+			print j
+			print >> fOut, '\''+ fileList[(i*100)+j ][:-1] +'\''
 			
-	print ' )'
-	print ')'
-	print 'process.demo = cms.EDAnalyzer(\'NtupleMaker\''
-	print   ', tracks = cms.untracked.InputTag(\'generalTracks\'),'
-	print   'outFile = cms.string("'+outputFile+str(i+1)+'.root'+'")'
-	print  ')'
-	print 'process.p = cms.Path(process.demo)'
+	print >> fOut, ' )'
+	print >> fOut,')'
+	print >> fOut,'process.demo = cms.EDAnalyzer(\'NtupleMaker\''
+	print  >> fOut, ', tracks = cms.untracked.InputTag(\'generalTracks\'),'
+	print  >> fOut, 'outFile = cms.string("'+outputFile+str(i)+'.root'+'")'
+	print >> fOut, ')'
+	print>> fOut, 'process.p = cms.Path(process.demo)'
+	
+	
+print count
 
 
 
