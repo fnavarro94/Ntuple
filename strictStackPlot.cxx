9b4a9b8f7b92ed5cd393100bc141e2d5ef6a639d
Felipe Navarro <felinavarro94@gmail.com>
2018-05-28 13:14:37 -0500

    saving

--- strictStackPlot.cxx
+++ strictStackPlot.cxx
@@ -1,38 +1,36 @@
 {
+	
+	double lumi = 5100.227976804;  //   1/pb
+	 
+	
    THStack *hs = new THStack("hs","");
    THStack *hs_mass = new THStack("hs_mass","");
   
-   TFile *fDY = new TFile("strictDYTMuon.root");
-   // TFile *fZZ2 = new TFile("histsZZ2.root");
-   TFile *fZZT = new TFile("strictZZMuon.root");
-   // TFile *fZZ4 = new TFile("histsZZ4.root");
-   TFile *fWZ = new TFile("strictWZMuon.root");
-   TFile *fWW = new TFile("strictWWMuon.root");
-   TFile *fDat = new TFile("hists.root");
+   TFile *fDY = new TFile("DYbg.root");
+   TFile *fZZT = new TFile("ZZbg.root");
+   TFile *fWZ = new TFile("WZbg.root");
+   TFile *fWW = new TFile("WWbg.root");
+   TFile *fDat = new TFile("lw.root");
    
-   TH1F * hDY = (TH1F*)fDY->Get("lxy_err");
-   //TH1F * hZZ2 = (TH1F*)fZZ2->Get("lxy_err");
+   TH1F * hDY = (TH1F*)fDY->Get("lxy_err"); 
    TH1F * hZZT = (TH1F*)fZZT->Get("lxy_err");
-   //  TH1F * hZZ4 = (TH1F*)fZZ4->Get("lxy_err");
    TH1F * hWZ = (TH1F*)fWZ->Get("lxy_err");
    TH1F * hWW = (TH1F*)fWW->Get("lxy_err");
    TH1F * hDat = (TH1F*)fDat->Get("lxy_err");
    
    
    TH1F * hDY_mass = (TH1F*)fDY->Get("InvMass");
-   //TH1F * hZZ2 = (TH1F*)fZZ2->Get("lxy_err");
    TH1F * hZZT_mass = (TH1F*)fZZT->Get("InvMass");
-   // TH1F * hZZ4 = (TH1F*)fZZ4->Get("lxy_err");
    TH1F * hWZ_mass = (TH1F*)fWZ->Get("InvMass");
    TH1F * hWW_mass = (TH1F*)fWW->Get("InvMass");
    TH1F * hDat_mass = (TH1F*)fDat->Get("InvMass");
    
+   
+   
    hDY->Sumw2();
    hDY_mass->Sumw2();
-   //hZZ2->Sumw2();
    hZZT->Sumw2();
    hZZT_mass->Sumw2();
-   //  hZZ4->Sumw2();
    hWZ->Sumw2();
    hWZ_mass->Sumw2();
    hWW->Sumw2();
@@ -42,11 +40,10 @@
    
    hDY->SetFillColor(2);
    hDY_mass->SetFillColor(2);
-   //hZZ2->SetFillColor(8);
    hZZT->SetFillColor(kGreen +3);
    hZZT_mass->SetFillColor(kGreen +3);
   
- //  hZZ4->SetFillColor(2);
+  
    hWZ->SetFillColor(kCyan -9);
    hWZ_mass->SetFillColor(kCyan -9);
    hWW->SetFillColor(kMagenta -3);
@@ -54,10 +51,10 @@
    hDat->SetFillColor(1);
    hDat_mass->SetFillColor(1);
    
-  double DY_scale = 17568*3048.0/(20987986);
-  double ZZ_scale = 17568*5.9/(3899263);
-  double WZ_scale = 17568*18.2/(1284698);
-  double WW_scale = 17568*42.0/(193524);
+  double DY_scale = lumi*3048.0/(20987986);
+  double ZZ_scale = lumi*5.9/(3899263);
+  double WZ_scale = lumi*18.2/(1284698);
+  double WW_scale = lumi*42.0/(193524);
   double Dat_scale =1.0;
    
    hDY->Scale(DY_scale);
@@ -79,7 +76,8 @@
    hWW_mass->SetStats(0);
    hDat_mass->SetStats(0);
    
-   //hs->Add(hZZ2);
+  
+  
      hs->Add(hZZT);
      hs->Add(hWZ);
      hs->Add(hWW);
@@ -94,7 +92,7 @@
     
     
    
-  // hs->Add(hZZ4);
+
    
  
    
@@ -119,8 +117,8 @@
     hDat_mass->SetXTitle("Mass [GeV/C^2]");
     hDat_mass->SetYTitle("Entries");
     hDat_mass->SetTitle("Muon Pair Invariant Mass");
-   //cs->cd(1); hDat->Draw("e"); cs->Update(); hs->Draw("same hist");T.DrawTextNDC(.5,.95,"Default drawing option"); 
- hDat_mass->Draw("e"); cs->Update(); hs_mass->Draw("same hist"); hDat_mass->Draww("e same")
+   cs->cd(1); hDat->Draw("e"); cs->Update(); hs->Draw("same hist");T.DrawTextNDC(.5,.95,"Default drawing option"); 
+ //hDat_mass->Draw("e"); cs->Update(); hs_mass->Draw("same hist"); hDat_mass->Draww("e same")
 legend->Draw(); 
    gPad->Update();
    gPad->SetLogy(1);
