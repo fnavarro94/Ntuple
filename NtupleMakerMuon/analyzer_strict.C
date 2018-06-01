78a1c6635942282cd18e16b06e49301a59b380da
Felipe Navarro <felinavarro94@gmail.com>
2018-05-31 19:55:42 -0500

    muon analyzer updated

--- NtupleMakerMuon/analyzer_strict.C
+++ NtupleMakerMuon/analyzer_strict.C
@@ -47,14 +47,34 @@ void analyzer_strict::SlaveBegin(TTree * /*tree*/)
    TString option = GetOption();
    
    file = new TFile("exotic.root", "recreate");
+   
+   h_invMassLoose = new TH1F ("InvMassLoose", "Lepton Pair Invariant Mass (loose)", 100, 0 , 600);
    h_invMass = new TH1F ("InvMass", "Lepton Pair Invariant Mass", 100, 0 , 600);
+   h_invMassLW = new TH1F ("InvMassLW", "Lepton Pair Invariant Mass (L-W cuts)", 100, 0 , 600);
    h_lxy = new TH1F ("lxy", "Transverse decay length", 20, 0 , 20);
+   h_lxyLoose = new TH1F ("lxyLoose", "Transverse decay length (loose)", 20, 0 , 20);
    h_lxy_err = new TH1F ("lxy_err", "Transverse decay length significance", 20, 0 , 20);
+   h_lxy_errLoose = new TH1F ("lxy_errLoose", "Transverse decay length significance (loose)", 20, 0 , 20);
    h_lxy2_err = new TH1F ("lxy2_err", "Transverse decay length significance", 20, 0 , 20);
+   h_dxy_errLoose = new TH1F ("dxy_errLoose", "Impact parameter / Standar Deviation (loose)", 100, 0 , 20);
+   h_dxy_err = new TH1F ("dxy_err", "Impact parameter / Standar Deviation", 100, 0 , 20);
+   h_d0_errLoose = new TH1F ("d0_errLoose", "Impact parameter / Standar Deviation (loose)", 100, 0 , 20);
    h_d0_err = new TH1F ("d0_err", "Impact parameter / Standar Deviation", 100, 0 , 20);
    h_conePt = new TH1F ("conePt", "Transverse momentum sum arround isolation cone", 100, 0 , 20);
+   h_cos = new TH1F ("cos", "Cos(#alpha) lepton pairs", 100, -1.1 , 1.1);
+   h_cosLoose = new TH1F ("cosLoose", "Cos(#alpha) lepton pairs (loose)", 100, -1.1 , 1.1);
+   h_delPhi = new TH1F ("delPhi", "lep Pt and vertex distance vector angle", 100, 0 , 1);
+   h_delPhiLoose = new TH1F ("delPhiLoose", "lep Pt and vertex distance vector angle (loose)", 100, 0 , 1);
+   h_chi2_NDF = new TH1F ("chi2_NDF", "#chi^{2}/NDF", 100, 0 , 20);
+   h_chi2_NDFLoose = new TH1F ("chi2_NDFLoose", "#chi^{2}/NDF (loose)", 100, 0 , 20);
+   h_numHitsLoose = new TH1F ("numHitsLoose", "Number of tracker hits between lepton pairs", 100, 0 , 20);
+   h_dot = new TH1F ("h_dot","Dot product between lepton pair momentum and secVert-primVert distance",100,-10000,10000);
    nEvents = new TH1F ("nEvents", "Number of Events", 5, -5,5);
-  //matchedTrack[ = {0};
+  
+   matchCount = 0;
+   triggerTurnOns = 0;
+   triggerObjects = 0;
+   effCount = 0;
    TH1::AddDirectory(true);
    vuelta = 0;
 }
@@ -84,25 +104,71 @@ bool standardCuts = cmsStandardCuts(vert_numTrack[0], Ev_Branch_numTrack, vertex
 reset();
 
 
+//  
+
+
+
+//
+
+
 //cout<<"number of trigger objects "<<Ev_Branch_numTrigObjM<<endl;
 
 nEvents->Fill(1); 
+if (triggerMActivated)
+{
+	triggerTurnOns++;
+}
 
-if (standardCuts && triggerActivated)   // quitar true
+
+//****** LeeWick mod
+
+     /*                  
+int ctemp = 0;
+int check = 0;
+for (int i = 0; i< Ev_Branch_numTrack; i++)
+{
+	
+
+	track_px[i] = -track_px[i];
+	track_py[i] = -track_py[i];
+	track_pz[i] = -track_pz[i];
+	
+	//if (track_nHits[i] > 5 && track_n3DHits[i]>1)
+	if(track_pt[i]>33)
+	{  
+		  // cout<<track_nHits[i]<<endl;
+	
+			ctemp++;
+		
+	}
+	
+	
+	
+}
+
+if (ctemp >  1)
+{
+	effCount++;
+}
+
+*/
+triggerObjects = triggerObjects + Ev_Branch_numTrigObj;
+if (standardCuts && triggerMActivated)   // quitar true
 {   
-
+    
+     
 	for (int i = 0 ; i< Ev_Branch_numTrack; i++)
 	{
 
-		for (int j = 0; j< Ev_Branch_numTrigObjM; j++)
+		for (int j = 0; j< Ev_Branch_numTrigObj; j++)
 		{
-			bool lepMatch =matchingCuts( track_highPurity[i]  , track_pt[i] , track_found[i], fabs(track_eta[i]), fabs(track_dxy[i]/track_dxyError[i]));
+			bool lepMatch =matchingCuts( track_highPurity[i]  , track_pt[i] , track_nHits[i],track_n3DHits[i], fabs(track_eta[i]));
 		
 		  
-		   if (lepMatch||true)
+		   if (lepMatch)
 		   {  
-			   if(deltaR(track_phi[i], track_eta[i], trigObj_phi[j], trigObj_eta[j])< 0.1 /*&& deltaP(track_px[i], track_py[i],track_pz[i], trigObj_px[j], trigObj_py[j], trigObj_pz[j]) < 3*/)
-			   {   
+			   if(deltaR(track_phi[i], track_eta[i], trigObj_phi[j], trigObj_eta[j])< 0.1 )
+			   {   matchCount++;
 				   matchedTrack[i] = 1;
 			       matchedTrigObj[j] = (matchedTrigObj[j] + 1)%2;
 			       trackTrigObjIndex[i] = j;
@@ -125,30 +191,98 @@ if (standardCuts && triggerActivated)   // quitar true
 			for (int j =0; j< Ev_Branch_numTrack; j++)
 			if ( matchedTrack[j] == 1 && track_charge[j] == -1 && trackTrigObjIndex[i] != trackTrigObjIndex[j] && deltaR(track_phi[i], track_eta[i], track_phi[j], track_eta[j]) >0.2)
 			{  
-				if ( deltaV(track_vx[i], track_vy[i], track_vz[i],track_vx[j], track_vy[j], track_vz[j]) < 3 )
+				if ( deltaV(track_vx[i], track_vy[i], track_vz[i],track_vx[j], track_vy[j], track_vz[j]) <5 )
 				{       
 					double conePt_var = conePt(i, j, track_eta[i], track_phi[i], Ev_Branch_numTrack, track_eta, track_phi, track_pt);
 					double alpha = mCos(track_phi[i], track_eta[i], track_phi[j], track_eta[j]);
 					double theta = mTheta(track_px[i]+track_px[j], track_py[i]+track_py[j],vertex_x[0]-track_vx[i],  vertex_y[0]-track_vy[i]); 
 					double theta2 = mTheta(-track_px[i]-track_px[j], -track_py[i]-track_py[j],vertex_x[0]-track_vx[i],  vertex_y[0]-track_vy[i]);
+					double cosAlpha = mCos(track_phi[i], track_eta[i], track_phi[j], track_eta[j]);
+					
+					double dot, iM;
+					
+					double p1[3], p2[3], sv1[3], sv2[3], pV[3];
+					
+					
                          	   	
 					//cout<<conePt_var<<endl;
 					//cout<<alpha<<endl;
 					//cout<<theta*180/(3.1415)<<endl;
 					
-					if (conePt_var < 4 && alpha > -0.95 && (theta < 0.2 )/*0.8 ipara electron*/)
+					//if (conePt_var < 4 && alpha > -0.95 && (theta < 0.2 )/*0.8 ipara electron*/)
+					if (conePt_var < 4 && alpha > -0.95/*0.8 ipara electron*/)
+					
 					{
 						double invariantMass, sumPt;
 					 invariantMass = invMass(track_px[i], track_py[i], track_pz[i], track_px[j], track_py[j], track_pz[j]);
-					 //cout<<invariantMass<<" "<<theta*180/(3.1415)<<endl;
-					 //cout<<track_lxy1[i]<<endl;
-					 //cout<<sqrt((track_vx[i]-vertex_x[0])*(track_vx[i]-vertex_x[0])+(track_vy[i] -vertex_y[0])*(track_vy[i] -vertex_y[0]))<<"   "<<track_lxy1[i]<<" "<<abs(sqrt((track_vx[i]-vertex_x[0])*(track_vx[i]-vertex_x[0])+(track_vy[i] -vertex_y[0])*(track_vy[i] -vertex_y[0]))-track_lxy1[i])/track_lxy1[i]<<endl;
-					 //cout<<sqrt((track_vx[i]-vertex_x[0])*(track_vx[i]-vertex_x[0])+(track_vy[i] -vertex_y[0])*(track_vy[i] -vertex_y[0]))<<"   "<<track_lxy1[i]<<" "<<abs(sqrt((track_vx[i]-vertex_x[0])*(track_vx[i]-vertex_x[0])+(track_vy[i] -vertex_y[0])*(track_vy[i] -vertex_y[0]))-track_lxy1[i])/track_lxy1[i]<<endl;
-					 h_invMass->Fill(invariantMass);
-					 h_lxy->Fill(track_lxy1[i]);
-					 h_lxy_err->Fill(fabs(track_lxy1[i]/track_dxyError[i]));
-					 h_d0_err->Fill(fabs(track_dxy[i]/track_dxyError[i]));
-					 h_lxy2_err->Fill(fabs(track_lxy2[i]/track_dxyError[i]));
+					 
+					 
+					 
+					p1[0] = track_px[i];
+					p1[1] = track_py[i];
+					p1[2] = track_pz[i];
+					
+					p2[0] = track_px[i];
+					p2[1] = track_py[i];
+					p2[2] = track_pz[i];
+					
+					sv1[0] = track_vx[i];
+					sv1[1] = track_vy[i];
+					sv1[2] = track_vz[i];
+					
+					sv2[0] = track_vx[i];
+					sv2[1] = track_vy[i];
+					sv2[2] = track_vz[i];
+					
+					pV[0] = vertex_x[0];
+					pV[1] = vertex_y[0];
+					pV[2] = vertex_z[0];
+					dot = dotPL(pV,sv1,sv2,p1,p2);
+					
+					
+					
+					h_dot->Fill(dot);
+					 
+
+					//***************** Histograms with loose cuts
+					h_dot->Fill(dot); 
+					h_invMassLoose->Fill(invariantMass);
+					h_lxyLoose->Fill(track_lxy1[i]);
+					h_lxy_errLoose->Fill(fabs(track_lxy1[i]/track_dxyError[i]));
+					h_chi2_NDFLoose->Fill(track_chi2[i]/track_ndof[i]);
+					h_d0_errLoose->Fill(fabs(track_d0[i]/track_d0Error[i]));
+					h_dxy_errLoose->Fill(fabs(track_dxy[i]/track_dxyError[i]));
+					h_lxy2_err->Fill(fabs(track_lxy2[i]/track_dxyError[i]));
+					h_delPhiLoose->Fill(theta);
+					h_cosLoose->Fill(cosAlpha);
+					h_numHitsLoose->Fill(track_nHits[i]+track_nHits[j]);
+
+					 
+					 // ***************** Histograms with life time related cuts
+					 
+					 if (fabs(track_lxy1[i]/track_dxyError[i])>5 && fabs(track_dxy[i]/track_dxyError[i]) > 2)
+					 {
+							h_invMass->Fill(invariantMass);
+							h_lxy->Fill(track_lxy1[i]);
+							h_lxy_err->Fill(fabs(track_lxy1[i]/track_dxyError[i]));
+							h_chi2_NDF->Fill(track_chi2[i]/track_ndof[i]);
+							h_delPhi->Fill(theta);
+							h_cos->Fill(cosAlpha);
+							h_d0_err->Fill(fabs(track_d0[i]/track_d0Error[i]));
+							h_dxy_err->Fill(fabs(track_dxy[i]/track_dxyError[i]));
+							
+							if (dot<-400)
+							{
+								h_invMassLW->Fill(invariantMass);
+							}
+							
+							
+					 }
+					 
+					 
+					 //****************** End Histograms with....
+					 
+					 
 					}
 					
 	                                //cout<<invariantMass<<endl;				
@@ -167,7 +301,7 @@ if (standardCuts && triggerActivated)   // quitar true
    vuelta ++;
    if(vuelta%10000 == 0)
    {
-cout<<"Vuelta "<<vuelta<<endl;
+  cout<<"Vuelta "<<vuelta<<endl;
    }
 
    return kTRUE;
@@ -219,27 +353,22 @@ double analyzer_strict::conePt(int forbiddenIndex1, int forbiddenIndex2, double 
 
 // resets arrays and variables to Null/0
 
-bool analyzer_strict::matchingCuts( bool purity, double pt, int hits, double eta, double impSig)
+bool analyzer_strict::matchingCuts( bool purity, double pt, int hits, int hits3D, double eta)
 {
 	bool ret = false;
 	
 	
 		
-	  if(purity && pt > 33 && hits >= 6   && eta < 2 && impSig > 2 )
+	  if(purity && pt > 33 && hits >= 6   && eta < 2  )
+	  if(true)
+	  
 	  {
 		  ret = true;
 
 	  }	
 	 
 	  
-	
-/*	if(lepton == "electron")
-	{
-		if(purity && pt > 41 && hits >= 6  && eta < 2 && impSig > 3)
-		{
-			ret = true;
-		}
-	}*/
+
 	
 	return ret;
 }
@@ -250,7 +379,7 @@ void analyzer_strict::reset()
 	{
 		matchedTrack[i] = 0;
 	}
-	for (int i = 0; i< Ev_Branch_numTrigObjM; i++)
+	for (int i = 0; i< Ev_Branch_numTrigObj; i++)
 	{
 		matchedTrigObj[i] = 0;
 	}
@@ -305,6 +434,10 @@ double analyzer_strict::deltaV(double vx1, double vy1, double vz1, double vx2, d
 double analyzer_strict::deltaR(double obj1Phi, double obj1Eta, double obj2Phi, double obj2Eta)
 {
 	double dPhi = obj1Phi - obj2Phi;
+	if (abs(dPhi)>3.1415/2)
+	{
+		dPhi =  3.1415 -dPhi;
+	}
 	double dEta = obj1Eta - obj2Eta;
 	double dR = sqrt(dPhi*dPhi + dEta*dEta);
 	return dR;
@@ -347,8 +480,8 @@ bool analyzer_strict::cmsStandardCuts(Int_t numVertTracks, Int_t numTracks, Doub
 		
 	}
 	
-	//cout<<numTracks<<" "<< distCount<<" "<<highPurity<<" "<< moreThan25percent<<endl;
-	if (numTracks > 4 && distCount >4 && moreThan25percent)
+	
+	if (numTracks > 3 && distCount >3 &&  moreThan25percent)
 	{
 		ret  = true;
 	}
@@ -357,7 +490,36 @@ bool analyzer_strict::cmsStandardCuts(Int_t numVertTracks, Int_t numTracks, Doub
 	
 
 }
-
+double analyzer_strict::dotPL(double pV[3], double sv1[3], double sv2[3], double p1[3], double p2[3])
+{
+	double VAv[3], vertV[3], pTot[3], dot;
+	
+	for (int i = 0; i< 3; i++)
+	{  
+		pTot[i] = (p1[i] + p2[i]);
+		
+		VAv[i] = 0.5*(sv1[i] + sv2[i]);
+		
+		vertV[i] = VAv[i] - pV[i];
+	}
+	// normalize pAv
+	
+	for (int i = 0; i<3; i++)
+	{
+		/*pTot[i] = pTot[i]/norm(pTot[0],pTot[1], pTot[2]);
+		vertV[i] = vertV[i]/norm(vertV[0], vertV[1], vertV[2]);*/
+		
+		/*pTot[i] = pTot[i]/norm(pTot[0],pTot[1]);
+		vertV[i] = vertV[i]/norm(vertV[0], vertV[1]);*/
+	}
+	
+	
+	dot = pTot[0]*vertV[0] +pTot[1]*vertV[1] + pTot[2]*vertV[2];
+	//dot = pTot[0]*vertV[0] +pTot[1]*vertV[1];
+	//cout<<"normal "<<dot<<endl;
+	return dot;
+	
+}
 
 void analyzer_strict::SlaveTerminate()
 {
@@ -370,6 +532,10 @@ void analyzer_strict::SlaveTerminate()
 void analyzer_strict::Terminate()
 {
 	//cout<<vuelta<<endl;
+	cout<<"Number of matches: "<<matchCount<<" efficiency  "<<matchCount/(4000.0*4)<<endl;
+	cout<<"Trigger turnons: "<<triggerTurnOns<<" efficiency:   "<<triggerTurnOns/4000.0<<endl;
+	cout<<"NUmber of trigger objects "<<triggerObjects<<"     "<<triggerObjects/(4000.0*4)<<endl;
+	cout<<"eff Count: "<<effCount<<endl;
 	file->Write();
 	file->Close();
    // The Terminate() function is the last function to be called during
