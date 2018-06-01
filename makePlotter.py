066c5448876aab469acf5baaa09dc699147a33f9
Felipe Navarro <felinavarro94@gmail.com>
2018-05-31 22:19:26 -0500

    script to make plotter.cxx added

---
+++ makePlotter.py
@@ -0,0 +1,72 @@
+
+
+
+paramList = ["lxy","lxyLoose", "lxy_err","lxy_errLoose", "InvMass", "InvMassLoose", "InvMassLW", "d0_err", "d0_errLoose","dxy_err", "dxy_errLoose", 
+               "cos","cosLoose", "delPhi", "delPhiLoose","conePt", "chi2_NDF","chi2_NDF", "chi2_NDFLoose", "numHitsLoose", "h_dot"]
+
+dataSetList = ["ZZ", "WZ", "WW","DY"]
+dataSetColor = ["kGreen +3", "kCyan -9","",""]
+otherSetList = ["ll","Dat"]
+mfile = open('plotter.cxx','w')
+
+#THStacks
+
+
+print >> mfile, "{ \n"
+
+print >>mfile, "double lumi = 5100.227976804;  //   1/pb" + "\n"
+
+for param in paramList:
+	print >> mfile, "THStack *hs_" + param + " = new THStack(\"hs_"+  param + "\", \"\");"
+	
+print>> mfile, " \n \n"
+
+for dset in dataSetList:
+	print >> mfile, "TFile *f"+dset+" = new TFile(\""+dset+"bg.root\");"
+	
+for dset in otherSetList:
+	print >> mfile, "TFile *f"+dset+" = new TFile(\""+dset+".root\");"
+	
+print>> mfile, " \n \n"
+
+for param in paramList:
+	for dset in dataSetList:
+		
+		print >> mfile, "TH1F * h"+dset+"_"+param+ "= (TH1F*)f"+dset+"->Get(\""+param+"\");" 
+		#TH1F * hDY_lx_err = (TH1F*)fDY->Get("lxy_err"); 
+	print >> mfile , "\n"
+for param in paramList:
+	for dset in otherSetList:
+		
+		print >> mfile, "TH1F * h"+dset+"_"+param+ "= (TH1F*)f"+dset+"->Get(\""+param+"\");" 
+		#TH1F * hDY_lx_err = (TH1F*)fDY->Get("lxy_err"); 
+	print >> mfile , "\n"
+
+for param in paramList:
+	for dset in dataSetList:
+			print >> mfile , "h"+dset+"_"+param+"->Sumw2();"
+		
+	print >> mfile, "\n"
+	
+for param in paramList:
+	for dset in otherSetList:
+			print >> mfile , "h"+dset+"_"+param+"->Sumw2();"
+		
+	print >> mfile, "\n"
+	
+	
+for param in paramList:
+	for dset in dataSetList:
+			print >> mfile , "h"+dset+"_"+param+"->SetLineColor(1);"
+		
+	print >> mfile, "\n"
+	
+for param in paramList:
+	for dset in dataSetList:
+			print >> mfile , "h"+dset+"_"+param+"->SetFillColor("+color+");"
+		
+	print >> mfile, "\n"
+
+
+
+print >> mfile, " \n }"
